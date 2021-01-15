67be2a3d5aba0403667715bdb8f734bfc38f7003
Vera Ueda <veraueda@hotmail.com>
2021-01-15 00:16:18 -0300

    Selected products

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -6,11 +6,18 @@ import { fetchProducts } from '../api';
 import { Product } from './types';
 import OrderLocation from './OrderLocation';
 import { OrderLocationdata } from './types'
+import OrderSummary from './OrderSummary';
+import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price;
+    }, 0);
 
     useEffect(() => {
         fetchProducts()
@@ -18,12 +25,34 @@ function Orders() {
         .catch(error => console.log(error))
     }, []);
 
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product);
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }
+
     return (
-       <div className = "orders-container">
-           <StepsHeader />
-           <ProductsList products = {products} />
-           <OrderLocation onChangeLocation={location => setOrderLocation(location)} />        
-       </div>
+        <>
+            <div className = "orders-container">
+                <StepsHeader />
+                <ProductsList 
+                products = {products}
+                onSelectProduct = {handleSelectProduct}
+                selectedProducts = {selectedProducts}
+                />
+                <OrderLocation onChangeLocation={location => setOrderLocation(location)} 
+                />
+                <OrderSummary 
+                amount={selectedProducts.length}
+                totalPrice={totalPrice} />      
+            </div>
+            <Footer />
+       </>
     )
 }
 export default Orders;
